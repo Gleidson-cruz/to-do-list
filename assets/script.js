f87bbfdddeebd9c676ac62b559a754639a716398
Gleidson-cruz <gleidsontech@gmail.com>
2024-12-11 13:37:29 -0300

    pouco progresso no darkmode

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,36 @@
-
 /*DOM INICIO */
 
-document.getElementById("modeSelect").innerHTML = "<span id='lightMode' class='navbar-brand mb-0 h1'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#fff' class='bi bi-lightbulb-fill' viewBox='0 0 16 16'><path d='M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5'/></svg></span>"
 
-document.getElementById("modeSelect").addEventListener("click", function() {
-  modeSelect.innerHTML = "<span id='darkMode' class='navbar-brand mb-0 h1'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#fff' class='bi bi-lightbulb' viewBox=' 0 0 16 16'><path d='M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1'/></svg></span>"
+document.addEventListener("DOMContentLoaded", function () {
+  let darkOn = true;
+  const light =
+    "<span id='lightMode' class='mode navbar-brand mb-0 h1'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#000' class='bi bi-lightbulb-fill' viewBox='0 0 16 16'><path d='M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5'/></svg></span>";
+  const dark =
+    "<span id='darkMode' class='mode navbar-brand mb-0 h1'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#fff' class='bi bi-lightbulb-fill' viewBox='0 0 16 16'><path d='M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5'/></svg></span>";
+  const modeselect = document.getElementById("modeSelect");
+
+  modeselect.innerHTML = dark;
+
+  document.getElementById("darkMode").addEventListener("click", function() {
+    if (darkOn === true) {
+      darkOn = false;
+      modeselect.innerHTML = light;
+      console.log(darkOn)
+    } 
+
+    document.getElementById("lightMode").addEventListener("click", function() {
+      if (darkOn === false) {
+        darkOn = true;
+        modeselect.innerHTML = dark;
+        console.log(darkOn)
+      }
+    }
+    )
+  })
+
 })
 
-
-
-
-
 /*DOM FIM */
-
 
 const btnAdcionar = document.getElementById("adicionar-tarefa");
 
